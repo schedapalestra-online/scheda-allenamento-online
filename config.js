edc29b4775f3c4bed926e2526688eb5a0a62787a
schedapalestra-online <myschedapalestra.online@gmail.com>
2026-08-03 21:37:01 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAmK7VbQs6EPq4FpHIDlIvtRKKrsOtszLc",
+  authDomain: "scheda-allenamento-peronale.firebaseapp.com",
+  projectId: "scheda-allenamento-peronale",
+  storageBucket: "scheda-allenamento-peronale.firebasestorage.app",
+  messagingSenderId: "30141517918",
+  appId: "1:30141517918:web:0cb63350572ab3121b64cf"
+};
